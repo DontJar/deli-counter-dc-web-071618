ed9cb35472f917a63f4e70926522a20039dc8b0f
Jon Schulman <schulman99@gmail.com>
2018-06-25 20:43:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,9 @@ def line(current_line)
     if current_line == 0
       puts "There is nobody waiting to be served!"
     else current_line.each do |person|
-      line << "#{i-1}. #{person}"
+      line << "#{i+1}. #{person}"
       i+1
       puts "The line is currently #{line}."
+    end
+  end
+end
