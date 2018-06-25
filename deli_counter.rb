8e10fc8ded4857ff4f779c58ca192155043c3849
Jon Schulman <schulman99@gmail.com>
2018-06-25 20:44:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(current_line)
   i = 0
   line = []
     if current_line == 0
-      puts "There is nobody waiting to be served!"
+      puts "The line is currently empty."
     else current_line.each do |person|
       line << "#{i+1}. #{person}"
       i+1
