5ea4bba4ba6146cdbc71884fc5a037bcec197534
Jon Schulman <schulman99@gmail.com>
2018-06-25 21:00:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 def line(current_line)
+  i=0
     if current_line.size == 0
       puts "The line is currently empty."
     else
-      i = 0
       line = []
       current_line.each do |person|
       line << "#{i+1}. #{person}"
