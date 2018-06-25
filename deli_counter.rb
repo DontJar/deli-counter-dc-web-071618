027a84174b6b42f070337552a14425e7133f1d7c
Jon Schulman <schulman99@gmail.com>
2018-06-25 20:54:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(current_line)
       current_line.each do |person|
       line << "#{i+1}. #{person}"
       i+1
-      puts "The line is currently: #{line}."
     end
+      puts "The line is currently: #{line}."
   end
 end
